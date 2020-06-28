39310626bd59191463fa204c14748a0093e33e4c
Nithin <sainithin0821@gmail.com>
2020-06-28 23:56:04 +0530

    Added algorithm

--- Pomo.py
+++ Pomo.py
@@ -1,1 +1,11 @@
 #File to implement personal pomodoro technique
+
+'''
+Algorithm
+#Take in 'start' keyword
+#Start time
+#Wait for stop keyword
+#Calculate time
+#Ask for category
+#Ask for what work was done
+#Save it to csv file
