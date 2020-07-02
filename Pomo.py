71bbbc5b609b0a339d57d0e31cb6220dc0990181
Nithin <sainithin0821@gmail.com>
2020-07-02 23:53:12 +0530

    Major changes

--- Pomo.py
+++ Pomo.py
@@ -14,21 +14,23 @@ Algorithm
 from datetime import datetime as d
 
 #startprint(d.now())
+print("Enter 'start' when you are ready!!")
 n = input()
 if n == 'start':
-    start = d.now().time()
-
+    start = d.now()
+print("Stop wasting time now!\nHead to work.\n\nType 'stop' when you are done with your session.")
 n = input()
-stop = d.now().time()
 
-hours = start.hour - stop.hour
-minutes = start.minute - stop.minute
-duration = str(hours)+' hrs '+str(minute)+" minutes "
+stop = d.now()
+hours = stop.hour - start.hour
+minutes = stop.minute - start.minute
+duration = str(stop - start)
+#duration = str(hours)+' hrs '+str(minutes)+' minutes '
 print("Enter category:")
 category = input()
 print('Enter the activity:')
 activity = input()
 
 with open('Progress.csv',"a") as file:
-    record = start+','+stop+','+duration+','+category+','+activity+'\n'
+    record = str(start)+','+str(stop)+','+duration+','+category+','+activity+'\n'
     file.write(record)
