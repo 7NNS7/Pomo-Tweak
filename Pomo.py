c454c516a3ac1ae0b4b50a69052b2fd73fe54940
Nithin <sainithin0821@gmail.com>
2020-07-07 15:52:06 +0530

    Easier usage of keys

--- Pomo.py
+++ Pomo.py
@@ -23,13 +23,10 @@ except:
     file.close()
 
 
-print("Enter 'start' when you are ready!!")
+print("Hit Enter when you are ready!!")
+start = d.now()
+print("Stop wasting time now!\nHead to work.\n\nHit Enter when you are done with your session.")
 n = input()
-if n == 'start':
-    start = d.now()
-print("Stop wasting time now!\nHead to work.\n\nType 'stop' when you are done with your session.")
-n = input()
-
 stop = d.now()
 hours = stop.hour - start.hour
 minutes = stop.minute - start.minute
