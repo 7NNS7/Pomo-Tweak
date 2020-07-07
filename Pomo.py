878d75b4d22b6caae0f701aed321203bd466b462
Nithin <sainithin0821@gmail.com>
2020-07-07 15:53:16 +0530

    Changes

--- Pomo.py
+++ Pomo.py
@@ -24,6 +24,7 @@ except:
 
 
 print("Hit Enter when you are ready!!")
+n = input()
 start = d.now()
 print("Stop wasting time now!\nHead to work.\n\nHit Enter when you are done with your session.")
 n = input()
